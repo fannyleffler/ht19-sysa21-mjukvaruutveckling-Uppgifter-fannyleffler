b941e78931073ac6b736331dd3f1a657e47f53aa
fannyleffler <56675257+fannyleffler@users.noreply.github.com>
2020-01-17 13:56:21 +0100

    Alla inl förutom 6

---
+++ Inlamning3/src/a.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class a {
+	
+	public static void main (String[]args) {
+		
+		int tal = 0;
+		int summa = 0;
+		int medelvarde = 0;
+		int max;
+		int[] lista = new int [5];
+		
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Skriv in fem tal: ");
+		
+		for (int i = 0; i < 5; i++) {
+			tal = scan.nextInt();
+			lista[i] = tal;
+		}
+			max = lista[0];
+		for (int i = 0; i < 5; i++) {
+			summa += lista[i];
+			if (max < lista[i]) {
+				max = lista[i];
+			}
+		}
+			medelvarde = (summa / lista.length);
+		System.out.println("Summan av de fem talen är: " + summa);
+		System.out.println("Medelvärdet av de fem talen är: " + medelvarde);
+		System.out.println("Det största av de fem talen är: " + max);
+		
+	}
+
+}
---
+++ Inlamning5/src/Test.java
@@ -0,0 +1,43 @@
+public class Test {
+
+	public static void main(String[] args) {
+		Account bankAccount1 = new Account();
+		Account bankAccount2 = new Account();	
+		
+		bankAccount1.setBalance(2000.5);
+		bankAccount1.setNr("123");
+		bankAccount2.setBalance(5000.5);
+		bankAccount2.setNr("456");
+		
+		Person person1 = new Person();
+		Person person2 = new Person();
+		
+		person1.setPnr("111111-1111");
+		person1.setName("Anna");
+		
+		person2.setPnr("222222-2222");
+		person2.setName("Peter");
+		
+		bankAccount1.setOwner(person1);
+		bankAccount2.setOwner(person2);
+		
+		PersonRegister personer = new PersonRegister();
+		personer.addPerson(person1);
+		personer.addPerson(person2);
+		
+		System.out.println(bankAccount1.getOwner().getName());
+		System.out.println(person2.getName());
+	
+		Person p = personer.findPerson("111111-1111");
+			if (p != null) {
+				System.out.println("Hittade: " + p.getName());
+		} 
+		personer.removePerson("111111-1111");
+		Person a = personer.findPerson("111111-1111");
+			if (a == null) {
+				System.out.println("Fungerar!");
+			}
+		
+
+	}
+}
